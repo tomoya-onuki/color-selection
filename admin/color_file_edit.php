dc5794248be5ab1abfcd4996762ded688625bc50
tomoya-onuki <tomoya.onuki28@gmail.com>
2019-11-09 19:48:19 +0900

    delete機能完成

--- admin/color_file_edit.php
+++ admin/color_file_edit.php
@@ -22,7 +22,7 @@ $pdo = new PDO($dsn, $url['user'], $url['pass']);
 			<div class=header>
 				<a href="index.php"><h1>COLOR SELECTION</h1></a>
 			</div>
-      <h2 style="margin-top:90px;">管理者用画面</h2>
+      <h2 style="margin-top:180px;">管理者用画面</h2>
 
 			<?php
 			$stmt = $pdo->prepare('SELECT * FROM color_tb');
