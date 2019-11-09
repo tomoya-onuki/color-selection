87074c7540cb4b193c8ddfb9d0fafb2595092c7f
tomoya-onuki <tomoya.onuki28@gmail.com>
2019-11-09 19:38:17 +0900

    delete機能追加2

---
+++ admin/color_file_edit.php
@@ -0,0 +1,57 @@
+<?php
+// データベースに接続
+$url = parse_url(getenv('DATABASE_URL'));
+$dsn = sprintf('pgsql:host=%s;dbname=%s', $url['host'], substr($url['path'], 1));
+$pdo = new PDO($dsn, $url['user'], $url['pass']);
+
+// データ取得
+
+?>
+
+<!DOCTYPE html>
+<html lang="ja" dir="ltr">
+	<head>
+		<meta charset="utf-8">
+		<title>COLOR FILE</title>
+		<link rel="stylesheet" type="text/css" href="./my.css">
+		<script src="http://code.jquery.com/jquery-latest.js"></script>
+    <script src="https://riversun.github.io/jsframe/jsframe.js"></script>
+    <script src="https://code.jquery.com/jquery-2.2.4.min.js" integrity="sha256-BbhdlvQf/xTY9gja0Dq3HiwQF8LaCRTXxZKRutelT44=" crossorigin="anonymous"></script>
+	</head>
+
+	<body>
+
+		<center>
+			<div class=header>
+				<a href="index.php"><h1>COLOR SELECTION</h1></a>
+			</div>
+
+			<?php
+			$stmt = $pdo->prepare('SELECT * FROM color_tb');
+			$stmt->execute();
+
+			// データをcolor変数に格納
+			$i = 0;
+			while ($colors = $stmt -> fetch(PDO::FETCH_ASSOC)) {
+				echo '<div>';
+				echo '<div class="color_file_box" style="background-color:'.$colors['color01'].'"></div>';
+				echo '<div class="color_file_box" style="background-color:'.$colors['color02'].'"></div>';
+				echo '<div class="color_file_box" style="background-color:'.$colors['color03'].'"></div>';
+				echo '</div>';
+				echo '<div style="margin-top:-5px; margin-bottom:20px;">';
+				echo '<div class="color_name">'.$colors['color01'].'</div>';
+				echo '<div class="color_name">'.$colors['color02'].'</div>';
+				echo '<div class="color_name">'.$colors['color03'].'</div>';
+        echo '<a href="delete.php?id='.$colors['id'].'">削除</a>';
+				echo '</div>';
+			}
+			?>
+
+			<!-- フッター -->
+      <div style="font-size: 70%; margin-top:30px; margin-bottom:80px;">
+        <p>©️2019 小貫智弥 | Tomoya Onuki</p>
+      </div>
+      <!-- ここまで -->
+		</center>
+	</body>
+</html>
--- admin/delete.php
+++ admin/delete.php
@@ -1,3 +1,17 @@
 <?php
-  echo "delete";
+// データベースに接続
+$url = parse_url(getenv('DATABASE_URL'));
+$dsn = sprintf('pgsql:host=%s;dbname=%s', $url['host'], substr($url['path'], 1));
+$pdo = new PDO($dsn, $url['user'], $url['pass']);
+
+// データ取得
+echo $_GET['id'];
+
+if ($_GET['id']) {
+  $stmt = $pdo->prepare('DELETE FROM color_tb WHERE id = :id');
+	$stmt->bindParam(':id', $_GET['id'], PDO::PARAM_STR);
+	$stmt->execute();
+}
+
+// header('location: color_file_edit.php');
 ?>
