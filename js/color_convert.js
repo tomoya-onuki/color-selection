fb05b5fc7da798843a624f09254baa2f892f79c6
tomoya-onuki <tomoya.onuki28@gmail.com>
2021-06-04 16:34:53 +0900

    new version

---
+++ js/color_convert.js
@@ -0,0 +1,180 @@
+function rgb2hsl(rgb) {
+	var r = rgb[0] / 255;
+	var g = rgb[1] / 255;
+	var b = rgb[2] / 255;
+
+	var max = Math.max(r, g, b);
+	var min = Math.min(r, g, b);
+	var diff = max - min;
+
+	var h = 0;
+	var l = (max + min) / 2 * 100;
+	var s = diff / (1 - (Math.abs(max + min - 1))) * 100;
+
+	switch (min) {
+		case max:
+			h = 0;
+			break;
+
+		case r:
+			h = Math.round((60 * ((b - g) / diff)) + 180);
+			break;
+
+		case g:
+			h = Math.round((60 * ((r - b) / diff)) + 300);
+			break;
+
+		case b:
+			h = Math.round((60 * ((g - r) / diff)) + 60);
+			break;
+	}
+
+	return [h, s, l];
+}
+
+function hsl2rgb ( hsl ) {
+	var h = hsl[0] ;
+	var s = hsl[1] ;
+	var l = hsl[2] ;
+
+	var max = l + ( s * ( 1 - Math.abs( ( 2 * l ) - 1 ) ) / 2 ) ;
+	var min = l - ( s * ( 1 - Math.abs( ( 2 * l ) - 1 ) ) / 2 ) ;
+
+	var rgb = [0, 0, 0];
+	var i = parseInt( h / 60 ) ;
+
+	switch( i ) {
+		case 0 :
+		case 6 :
+			rgb = [ max, min + (max - min) * (h / 60), min ] ;
+		break ;
+
+		case 1 :
+			rgb = [ min + (max - min) * (120 - h / 60), max, min ] ;
+		break ;
+
+		case 2 :
+			rgb = [ min, max, min + (max - min) * (h - 120 / 60) ] ;
+		break ;
+
+		case 3 :
+			rgb = [ min, min + (max - min) * (240 - h / 60), max ] ;
+		break ;
+
+		case 4 :
+			rgb = [ min + (max - min) * (h - 240 / 60), min, max ] ;
+		break ;
+
+		case 5 :
+			rgb = [ max, min, min + (max - min) * (360 - h / 60) ] ;
+		break ;
+	}
+
+	// return rgb.map( function ( value ) {
+	// 	return value * 255 ;
+	// } ) ;
+
+	return rgb.map(v => Math.round(v));
+	// return [ rgb[0] * 255, rgb[1] * 255, rgb[2] * 255 ];
+}
+
+
+// -----------------------------
+// RGBをコードに変更
+// -----------------------------
+function rgb2hex ( rgb ) {
+	return "#" + rgb.map( function ( value ) {
+		return ( "0" + value.toString( 16 ) ).slice( -2 ) ;
+	} ).join( "" ) ;
+}
+// function rgb2hex(rgb) {
+// 	selectedColor[selectingPanelID] = "#";        // 初期化
+// 	for (var i = 0; i < 3; i++) {
+// 		var data = rgb[i].toString(16);             // 16進数に変換
+// 		if (data.length < 2) data = "0" + data;        // 0詰め処理
+// 		selectedColor[selectingPanelID] += data;    // カラーコードに変換
+// 	}
+// 	console.log(selectedColor[selectingPanelID]);
+// }
+// -----------------------------
+// -----------------------------
+
+
+
+// -----------------------------
+// hsbをrgbに変換する
+// -----------------------------
+function hsv2rgb(h, s, v) {
+	if (h < 0) {
+		h = h + Math.ceil(-h / 360) * 360;
+	} else if (h > 360) {
+		h = h % 360;
+	}
+
+	if (s < 0) { s = 0; }
+	else if (s > 255) { s = 255; }
+
+	if (v < 0) {
+		v = 0;
+	} else if (v > 255) {
+		v = 255;
+	}
+
+	if (s == 0) {
+		v = 0 | v;
+		return [v, v, v];
+	}
+
+	var r, g, b;
+	s /= 255;
+	var i = (0 | (h / 60)) % 6,
+		f = (h / 60) - i,
+		p = v * (1 - s),
+		q = v * (1 - f * s),
+		t = v * (1 - (1 - f) * s);
+	switch (i) {
+		case 0: r = v; g = t; b = p; break;
+		case 1: r = q; g = v; b = p; break;
+		case 2: r = p; g = v; b = t; break;
+		case 3: r = p; g = q; b = v; break;
+		case 4: r = t; g = p; b = v; break;
+		case 5: r = v; g = p; b = q; break;
+	}
+	rgb[selectingPanelID][0] = parseInt(r);
+	rgb[selectingPanelID][1] = parseInt(g);
+	rgb[selectingPanelID][2] = parseInt(b);
+}
+// -----------------------------
+// -----------------------------
+
+
+// -----------------------------
+// 16進数からrgbに変換
+// -----------------------------
+function hex2rgb(colorCode) {
+	var rgb = [];
+	rgb[0] = parseInt(colorCode.substr(1, 2), 16);
+	rgb[1] = parseInt(colorCode.substr(3, 2), 16);
+	rgb[2] = parseInt(colorCode.substr(5, 2), 16);
+	return rgb;
+}
+// -----------------------------
+// -----------------------------
+
+
+// -----------------------------
+// -----------------------------
+function rgb2hsb(rgb) {
+	var hsb = [];
+	var max = Math.max(rgb[0], rgb[1], rgb[2]);
+	var min = Math.min(rgb[0], rgb[1], rgb[2]);
+	if (min == rgb[0]) hsb[0] = 60 * (rgb[2] - rgb[1]) / (max - min) + 180;
+	if (min == rgb[1]) hsb[0] = 60 * (rgb[0] - rgb[2]) / (max - min) + 300;
+	if (min == rgb[2]) hsb[0] = 60 * (rgb[1] - rgb[0]) / (max - min) + 60;
+	hsb[2] = max;
+	hsb[1] = max - min;
+
+	return hsb;
+}
+// -----------------------------
+// -----------------------------
---
+++ js/color_selector2.js
@@ -0,0 +1,149 @@
+var COLOR_PALET_FLAG = -1;
+
+$(function () {
+    var hsl = init();
+    hue_circle();
+
+    /////////////////////////
+    // lightnessスライダーの操作
+    $('#lightness > input').on('input change', function () {
+        hsl[2] = parseInt($(this).val());
+        $('#lightness > .label >.value').text(hsl[2]);
+        // console.log(hsl);
+        setColor(hsl);
+    });
+
+    /////////////////////////
+    // saturationスライダーの操作
+    $('#saturation > input').on('input change', function () {
+        hsl[1] = parseInt($(this).val());
+        $('#saturation > .label >.value').text(hsl[1]);
+        // console.log(hsl);
+        setColor(hsl);
+    });
+
+    //////////////////
+    // hueサークルの操作
+    $('#hue > .item').each(function () {
+        $(this).on('click', function () {
+            var str = $(this).css('background');
+            const regexp = /\d+, \d+, \d+/g;
+            var matches_array = str.match(regexp);
+            var rgb = matches_array[0].split(', ');
+            var _hsl = rgb2hsl(rgb);
+            hsl[0] = _hsl[0];
+            setColor(hsl);
+        });
+    });
+
+
+    ////////////////////
+    // カラーパレットの選択
+    $('#color_palet0 > .color').on('click', function () {
+        $('.color').css('border-radius', '0px');
+        $(this).css('border-radius', '12px');
+        hsl = getColor( $(this) );
+        $('#saturation > input').val(hsl[1]);
+        $('#lightness > input').val(hsl[2]);
+        COLOR_PALET_FLAG = 0;
+    });
+    $('#color_palet1 > .color').on('click', function () {
+        $('.color').css('border-radius', '0px');
+        $(this).css('border-radius', '12px');
+        hsl = getColor( $(this) );
+        $('#saturation > input').val(hsl[1]);
+        $('#lightness > input').val(hsl[2]);
+        COLOR_PALET_FLAG = 1;
+    });
+    $('#color_palet2 > .color').on('click', function () {
+        $('.color').css('border-radius', '0px');
+        $(this).css('border-radius', '12px');
+        hsl = getColor( $(this) );
+        $('#saturation > input').val(hsl[1]);
+        $('#lightness > input').val(hsl[2]);
+        COLOR_PALET_FLAG = 2;
+    });
+});
+
+function setColor(hsl) {
+
+    var obj = null;
+    switch (COLOR_PALET_FLAG) {
+        case 0:
+            obj = $('#color_palet0');
+            break;
+        case 1:
+            obj = $('#color_palet1');
+            break;
+        case 2:
+            obj = $('#color_palet2');
+            break;
+    }
+
+    if (obj != null) {
+        var cssHsl = 'hsl(' + hsl[0] + ', ' + hsl[1] + '%, ' + hsl[2] + '%)';
+        obj.children('.color').css('background', cssHsl);
+        obj.find('.hsl').text(cssHsl);
+
+        var str = obj.find('.color').css('background');
+        // rgbで取得されるのでr,g,bを正規表現で抽出
+        const regexp = /\d+, \d+, \d+/g;
+        var token = str.match(regexp);
+        
+        // rgb(r,g,b)という形式に整形
+        var cssRgb = 'rgb(' + token[0] + ')';
+        obj.find('.rgb').text(cssRgb);
+        var rgb = token[0].split(', ');
+        var hex = rgb2hex(rgb);
+        obj.find('.hex').text(hex);
+    }
+
+
+}
+
+function getColor(obj) {
+    var str = obj.css('background');
+    const regexp = /\d+, \d+, \d+/g;
+    var token = str.match(regexp);
+    var rgb = token[0].split(', ');
+    return rgb2hsl(rgb);
+}
+
+function init() {
+    var l = parseInt($('#lightness > input').val());
+    $('#lightness > .label >.value').text(l);
+
+    var s = parseInt($('#saturation > input').val());
+    $('#saturation > .label >.value').text(s);
+
+    var h = 0;
+
+    setColor(h, s, l);
+    return [h, s, l];
+}
+
+
+function hue_circle() {
+    var baseColor = [
+        "#f9be01", "#f19601", "#ec6c01",
+        "#e4012e", "#a50082", "#561b85",
+        "#034fa4", "#006ab5", "#00a3ad",
+        "#00a93c", "#abcd06", "#fdd902",
+    ]; // 基本色
+
+    var item_num = $('#hue > .item').length;
+    var deg = 360.0 / item_num;
+    var red = (deg * Math.PI / 180.0);
+    var circle_r = $("#hue > .item").width() * 2.5;
+
+    $('#hue > .item').each(function (i) {
+        var x = Math.cos(red * i) * circle_r + circle_r;
+        var y = Math.sin(red * i) * circle_r + circle_r;
+        $(this).css('left', x);
+        $(this).css('top', y);
+        $(this).css('background', baseColor[i]);
+    });
+}
+
+
+
